76bf2e953524064a0ea966c5e0f5f95813ee0162
cdietrich-home <86250977+cdietrich-home@users.noreply.github.com>
2021-06-21 11:42:22 -0500

    Initial

---
+++ FindFirstDuplicate.psm1
@@ -0,0 +1,36 @@
+﻿function Get-FirstDuplicateInString
+{
+    <#
+    .SYNOPSIS 
+       Returns first matched duplicate letter in string else returns null
+     .DESCRIPTION
+        Takes parameter either through passed parameter or from pipe
+        Formatting is not PS standard but I find it works better for me to show matching brackets
+    .PARAMETER PassedString
+        String to evaluate
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [string]$PassedString
+    )
+    begin
+    {
+        $arr = New-Object System.Collections.Generic.HashSet[char]
+    }
+    process
+    {
+        for ($i = 0;  $i -lt $PassedString.Length; $i++)
+        {
+            $char = $PassedString[$i]
+            if ($arr.Contains($char))
+            {
+                return $char
+            }
+            else
+            {
+                $null = $arr.Add($char)
+            }
+        }
+    }
+} 
